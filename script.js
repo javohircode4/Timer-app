fa0ee5afe09ac0c96544f6115003ba7b3308aaf2
javohircode4 <javohirqosimiv23041999@gmail.com>
2026-03-24 23:26:19 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,79 @@
+const WEEK = ["SUN","MON","TUE","WED","THU","FRI","SAT"];
+
+function zero(num) {
+    return num < 10 ? "0" + num : num;
+}
+
+function updateTime() {
+    let now = new Date();
+
+    let hours = zero(now.getHours());
+    let minutes = zero(now.getMinutes());
+    let seconds = zero(now.getSeconds());
+
+document.getElementById("time").innerText =
+        hours + ":" + minutes + ":" + seconds;
+let timeEl = document.getElementById("time");
+
+timeEl.classList.remove("animate");
+void timeEl.offsetWidth;
+timeEl.classList.add("animate");
+let colors = ["cyan", "lime", "yellow", "magenta", "orange"];
+
+let randomColor = colors[Math.floor(Math.random() * colors.length)];
+
+document.getElementById("time").style.color = randomColor;
+    let year = now.getFullYear();
+    let month = zero(now.getMonth() + 1);
+    let day = zero(now.getDate());
+    let week = WEEK[now.getDay()];
+
+    document.getElementById("date").innerText =
+        year + "-" + month + "-" + day + " " + week;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
+function changeColor() {
+    let timeEl = document.getElementById("time");
+
+    // random rang
+    let colors = ["#ff0000", "#00ff00", "#00ccff", "#ff00ff", "#ffff00"];
+
+    let randomColor = colors[Math.floor(Math.random() * colors.length)];
+
+    timeEl.style.color = randomColor;
+    timeEl.style.textShadow = "0 0 20px " + randomColor;
+}
+function saveTime() {
+    let timeText = document.getElementById("time").innerText;
+
+    let container = document.getElementById("saved-times");
+
+    let newTime = document.createElement("div");
+    newTime.innerText = timeText;
+
+    container.appendChild(newTime);
+}
+const timeElement = document.getElementById("time");
+const savedContainer = document.getElementById("saved-times");
+
+function saveTime() {
+    const newTime = document.createElement("div");
+
+    newTime.innerText = timeElement.innerText;
+
+    // 🎨 rangni olish (tepdagi soatdan)
+    const color = window.getComputedStyle(timeElement).color;
+    const shadow = window.getComputedStyle(timeElement).textShadow;
+
+    // 🎯 pastga qo‘llash
+    newTime.style.color = color;
+    newTime.style.textShadow = shadow;
+
+    savedContainer.appendChild(newTime);
+}
+function resetTimes() {
+    const savedContainer = document.getElementById("saved-times");
+    savedContainer.innerHTML = "";
+}22:24 24.03.2026
